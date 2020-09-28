d63768f2a41bcae2b0a8539e7ca3016f372b278e
Suhas Neelam <suhasneelam38@gmail.com>
2020-09-28 17:33:01 +0530

    Second Commit

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ let hospCollection,ventCollection,db;
 
 MongoClient.connect(url)
   .then(client => {
-
     db = client.db('hospitalInventory');
     hospCollection = db.collection('hospitals');
     ventCollection = db.collection('ventilators');
@@ -30,12 +29,19 @@ app.get("/", (req, res) => {
   );
 });
 app.post('/hospitals', (req, res) => {
-  hospCollection.insertOne(req.body)
+  hospCollection.findOne({hname:req.body.hname})
   .then(result => {
-    console.log(result.result);
+    console.log("Already Exists");
     res.redirect('/');
   })
-  .catch(error => console.error(error))
+  .catch(() => {
+    hospCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result.result);
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+  })
 });
 app.get('/hospitals', (req, res) => {
   db.collection("hospitals").find().toArray()
@@ -58,6 +64,58 @@ app.delete('/hospitals', (req, res) => {
   })
   .catch(error => console.error(error));
 });
+
+
+app.post('/ventilators', (req, res) => {
+ let exists; 
+ db.collection("hospitals").findOne({ hname: req.body.hname })
+  .then(result => {
+    let no = result.hvent;
+    hospCollection.updateOne({ hname: req.body.hname }, { $set: { hvent: parseInt(no) + parseInt(req.body.ventno) } })
+      .then((obj) => {
+        res.redirect('/');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    ventCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result.result);
+        
+        res.redirect('/ventilators');
+      })
+      .catch(error => console.error(error))
+  })
+  .catch(err => {
+    hospCollection.insertOne({ hname: req.body.hname, hvent: req.body.ventno, haddress: Math.floor(Math.random() * 100) })
+      .then(result => {
+        console.log(result.result);
+      })
+      .catch(error => console.error(error))
+    ventCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result.result);
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+  });
+  
+});
+app.get('/ventilators', (req, res) => {
+  let total = 0;
+  db.collection("hospitals").find().toArray()
+    .then(results => {
+      results.forEach(element => {
+        total += parseInt(element.hvent);
+      });
+      console.log(total);
+      res.render('vents.ejs', { ventilators: total })
+    })
+    .catch(error => console.error(error))
+});
+
+
+
 
 app.listen(3000, function () {
   console.log("listening on 3000");
